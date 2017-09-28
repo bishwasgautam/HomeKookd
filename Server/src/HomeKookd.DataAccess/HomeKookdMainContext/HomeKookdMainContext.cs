81d37d69d02dce4db8432ebc0c16180fe421b602
Bishwas Gautam <bishwasgautam05@gmail.com>
2017-09-27 23:57:36 -0500

    Refactor move

--- Server/src/HomeKookd.DataAccess/HomeKookdMainContext/HomeKookdMainContext.cs
+++ Server/src/HomeKookd.DataAccess/HomeKookdMainContext/HomeKookdMainContext.cs
@@ -13,6 +13,16 @@ namespace HomeKookd.DataAccess
 
         public DbSet<Person> Persons { get; set; }
         public DbSet<User> Users { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Phone> Phones { get; set; }
+
+        public DbSet<Kook> Kooks { get; set; }
+        public DbSet<Kitchen> Kitchens { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+
+        public DbSet<Testimony> Testimonies { get; set; }
+        public DbSet<MealReview> MealReviews { get; set; }
+
 
     }
 }
